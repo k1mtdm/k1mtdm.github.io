b69ac9e31c1d123bbb0f5b2939909a74ada12d76
k1mtdm <131973345+k1mtdm@users.noreply.github.com>
2024-06-25 08:53:16 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://k1mtdm.github.io/',
+  site: 'https://k1mtdm.github.io/',
+  integrations: [tailwind()]
 });
