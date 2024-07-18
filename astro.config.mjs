502f86b6bb43e82fe93ee444435ce226a466ce20
k1mtdm <keiichi.mtdm@gmail.com>
2024-07-18 14:32:33 +0900

    build-test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://k1mtdm.github.io/',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind()],
 });
-
-module.exports = {
-  plugins: [
-    require('postcss-preset-env')({
-      stage: 1,
-      features: {
-        'nesting-rules': true
-      }
-    })
-  ]
-}
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
-    plugins: [
-      require('postcss-preset-env'),
-      require('autoprefixer'),
-    ],
-  };
+  plugins: [
+    require('postcss-preset-env')({
+      stage: 1,
+      features: {
+        'nesting-rules': true
+      }
+    })
+  ]
+}
